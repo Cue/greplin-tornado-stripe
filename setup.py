066e5700bdc0ea80130d7a0d066173ee8e4871a5
Robby Walker <robby.walker@gmail.com>
2011-01-13 22:25:03 -0800

    setup.py improvements

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
-from distutils.core import setup
-import os, sys
+#!/usr/bin/env python
+# Copyright 2010 Greplin, Inc.  All Rights Reserved.
 
-path, script = os.path.split(sys.argv[0])
-os.chdir(os.path.abspath(path))
+"""Setup script for Tornado compatible API for Stripe."""
 
-setup(name='devpayments-tornado',
+
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+
+
+setup(name='stripe-tornado',
       version='1.4.3',
-      description='/dev/payments tornado python bindings',
-      author='Greplin',
-      author_email='daniel@greplin.com',
-      url='http://www.greplin.com/',
-      packages=['devpayments'],
-      package_dir = {'devpayments' : 'src'}
+      description='Stripe tornado python bindings',
+      license='Apache',
+      author='Greplin, Inc.',
+      author_email='opensource@greplin.com',
+      url='http://www.github.com/Greplin/stripe-tornado',
+      packages=['stripe'],
+      package_dir = {'' : 'src'}
 )
 
