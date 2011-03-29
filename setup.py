45fcb900e2a9eaf3db9bc9df36529fb647a35f8d
Daniel Gross <danicgross@gmail.com>
2011-03-29 13:56:51 -0700

    fixed directory structure

--- setup.py
+++ setup.py
@@ -1,23 +1,41 @@
 #!/usr/bin/env python
-# Copyright 2010 Greplin, Inc.  All Rights Reserved.
+# Copyright 2011 The greplin-tornado-stripe Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-"""Setup script for Tornado compatible API for Stripe."""
 
+"""Setup script for greplin-tornado-stripe."""
 
 try:
   from setuptools import setup
 except ImportError:
   from distutils.core import setup
 
-
-setup(name='stripe-tornado',
-      version='1.4.3',
-      description='Stripe tornado python bindings',
+setup(name='greplin-tornado-ses',
+      version='0.1',
+      description='A client for the Stripe API',
       license='Apache',
       author='Greplin, Inc.',
       author_email='opensource@greplin.com',
-      url='http://www.github.com/Greplin/stripe-tornado',
-      packages=['stripe'],
-      package_dir = {'' : 'src'}
+      url='http://www.github.com/Greplin/greplin-tornado-stripe',
+      package_dir = {'':'src'},
+      packages = [
+        'greplin',
+        'greplin.tornado'
+      ],
+      namespace_packages = [
+        'greplin',
+        'greplin.tornado'
+      ],
+      zip_safe = True
 )
-
--- src/greplin/tornado/stripe.py
+++ src/greplin/tornado/stripe.py
@@ -1,16 +1,16 @@
-#  Copyright 2010 Greplin, Inc. All Rights Reserved.
+# Copyright 2011 The greplin-tornado-stripe Authors.
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#      http:#www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS-IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Author: Daniel Gross <daniel@greplin.com>
 # Based on the python bindings by Patrick Collison <patrick@collison.ie>
@@ -34,7 +34,7 @@ class Client(object):
   Usage (within a RequestHandler):
 
     def post(self):
-      devpay = devpayments.Client(self.settings['devpayments_secret'])
+      devpay = stripe.Client(self.settings['devpayments_secret'])
       sample_card = {
         'number': '4242424242424242',
         'exp_month': 10,
